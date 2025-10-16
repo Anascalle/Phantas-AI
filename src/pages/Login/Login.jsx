d2c6ccca6fcf64ef0e4a472c47930547f260fa25
FuzzyDuck1930 <141763142+FuzzyDuck1930@users.noreply.github.com>
2025-10-16 18:40:54 -0500

    Add files via upload

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,15 +2,24 @@
 import "./Login.css"
 export default function Login() {
     return (
-      <section className="auth">
-        <h2>Iniciar Sesión</h2>
-        <form>
-          <input type="email" placeholder="Correo electrónico" required />
-          <input type="password" placeholder="Contraseña" required />
-          <button type="submit">Entrar</button>
-        </form>
-      </section>
+      <section class="auth-card">
+      <h1>Phantas IA</h1>
+      <h2>Iniciar sesión</h2>
+
+      <form action="/login" method="POST" class="auth-form">
+        <label for="email">Correo electrónico</label>
+        <input type="email" id="email" name="email" required />
+
+        <label for="password">Contraseña</label>
+        <input type="password" id="password" name="password" required />
+
+        <button type="submit">Entrar</button>
+      </form>
+
+      <p class="auth-switch">
+        ¿No tienes cuenta?
+        <a href="register.html">Regístrate aquí</a>
+      </p>
+    </section>
     );
   }
-  
-  
